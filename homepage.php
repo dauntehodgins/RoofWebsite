a62a113d642789b3de23646f9455789ef372594c
dauntehodgins <dauntehodgins@gmail.com>
2025-02-24 11:58:40 -0800

    beginning of homepage

---
+++ homepage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>OverTheTop</title>
+<link rel="stylesheet" href="roof.css">
+</head>
+<header><a href="">Home</a> <a href="">Services</a> <a href="">Gallery</a> <a href="">Testimonials</a> <a href="">About us</a> <a href="">Contact us</a> <a href="">About us</a> <a href="">Free Estimate</a></header>
+<body><br><br>
+<h1>Over The Top Roofing</h1><br><br>
+<h2>Previous Work</h2>
+</body>
+</html>
