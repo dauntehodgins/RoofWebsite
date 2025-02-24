40ff66a7270ca306ba5340dd360f0119227e056b
dauntehodgins <dauntehodgins@gmail.com>
2025-02-24 12:16:46 -0800

    Add files via upload

--- homepage.php
+++ homepage.php
@@ -7,8 +7,8 @@
 <header><a href="">Home</a> <a href="">Services</a> <a href="">Gallery</a> <a href="">Testimonials</a> <a href="">About us</a> <a href="">Contact us</a> <a href="">About us</a> <a href="">Free Estimate</a></header>
 <body><br><br>
  <img src="overthetoplogo.png"> <br>
- <h3>KYLE CORMIER</h3><br>
- <h2>Mobile: 250-861-0520</h2><br>
- <h1>kyle@overthetopexterior.com</h1>
+ <h1>KYLE CORMIER</h1>
+ <h2>Mobile: 250-861-0520</h2>
+ <h3>kyle@overthetopexterior.com</h3>
 </body>
 </html>
